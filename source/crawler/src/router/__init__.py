517a637d62c7c15748aaabfa6adc72913c75e251
yangyang.cheng <yangyang.cheng@valueonline.cn>
2022-11-25 09:49:18 +0800

    update

--- source/crawler/src/router/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# file name: __init__.py
-# author: codepasser
-# date: 2022/11/11
-
-from src.router.router_app import AppRouter
-from src.router.router_debug import AppDebugRouter
-from src.router.router_sample import AppSampleRouter
